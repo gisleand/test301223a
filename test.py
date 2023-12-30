649ee209ad7c801633c509a9c72371d52452b6da
gisleand <153849522+gisleand@users.noreply.github.com>
2023-12-30 19:40:12 +0000

    more 2

--- test.py
+++ test.py
@@ -23,18 +23,36 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data)
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
+# map_data = pd.DataFrame(
+#  np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#   columns=['lat', 'lon'])
+# st.map(map_data)
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
 
-    chart_data
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+option2 = st.sidebar.caption(option)
+'You selected: ', option
